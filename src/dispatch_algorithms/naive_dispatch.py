a6abcb74204fbbf5bca8dc83ac52a68d290fd1ff
370025263 <370025263@qq.com>
2023-11-17 14:30:36 +0800

    add:naive dispatch algo for base env

--- src/dispatch_algorithms/naive_dispatch.py
+++ src/dispatch_algorithms/naive_dispatch.py
@@ -1,21 +1,22 @@
+from __future__ import annotations
+
 from sisymines.src.dispatcher import BaseDispatcher
-from sisymines.src.mine import Mine
-from sisymines.src.truck import Truck
+
 
 
 class NaiveDispatcher(BaseDispatcher):
     def __init__(self):
         super().__init__()
 
-    def give_init_order(self, truck: Truck, mine: Mine) -> int:
+    def give_init_order(self, truck: "Truck", mine: "Mine") -> int:
         # 从第一个load site开始
         return 0
 
-    def give_haul_order(self, truck: Truck, mine: Mine) -> int:
+    def give_haul_order(self, truck: "Truck", mine: "Mine") -> int:
         # 从第一个load site开始
         return 0
 
-    def give_back_order(self, truck: Truck, mine: Mine) -> int:
+    def give_back_order(self, truck: "Truck", mine: "Mine") -> int:
         # 从第一个dump site开始
         return 0
 
