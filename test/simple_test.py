bd767e44bfd4457744a3d028316b000ad0b1e6d0
370025263 <370025263@qq.com>
2023-12-20 15:32:54 +0800

    update: optimize testing runscript

--- test/simple_test.py
+++ test/simple_test.py
@@ -27,7 +27,7 @@ def main():
     for i in range(30):
         charging_site.add_truck(Truck(name=f"星火-油卡{i+1}", truck_capacity=30, truck_speed=25))
     for i in range(20):
-        charging_site.add_truck(Truck(name=f"抻录-电卡{i+1}", truck_capacity=120, truck_speed=20))
+        charging_site.add_truck(Truck(name=f"抻录-电卡{i+1}", truck_capacity=120, truck_speed=10))
     for i in range(20):
         charging_site.add_truck(Truck(name=f"官方-卡车{i+1}", truck_capacity=50, truck_speed=25))
 
@@ -37,15 +37,16 @@ def main():
     load_site_3 = LoadSite("北露天矿-装载点3")
 
     load_site_1.add_shovel(Shovel(name="北露天矿-装载点1-铲车1", shovel_tons=0.5, shovel_cycle_time=1))
-    load_site_1.add_shovel(Shovel(name="北露天矿-装载点1-铲车2", shovel_tons=2, shovel_cycle_time=2))
-    load_site_1.show_shovels()
+    #load_site_1.add_shovel(Shovel(name="北露天矿-装载点1-铲车2", shovel_tons=2, shovel_cycle_time=2))
 
     load_site_2.add_shovel(Shovel(name="北露天矿-装载点2-铲车1", shovel_tons=0.5, shovel_cycle_time=1))
-    load_site_2.add_shovel(Shovel(name="北露天矿-装载点2-铲车2", shovel_tons=0.5, shovel_cycle_time=2))
-    load_site_2.show_shovels()
+    #load_site_2.add_shovel(Shovel(name="北露天矿-装载点2-铲车2", shovel_tons=0.5, shovel_cycle_time=2))
 
     load_site_3.add_shovel(Shovel(name="北露天矿-装载点3-铲车1", shovel_tons=10, shovel_cycle_time=3))
-    load_site_3.add_shovel(Shovel(name="北露天矿-装载点3-铲车2", shovel_tons=10, shovel_cycle_time=3))
+    #load_site_3.add_shovel(Shovel(name="北露天矿-装载点3-铲车2", shovel_tons=10, shovel_cycle_time=3))
+
+    load_site_1.show_shovels()
+    load_site_2.show_shovels()
     load_site_3.show_shovels()
 
     # init dump_site and dumper
@@ -55,11 +56,12 @@ def main():
 
     for i in range(10):
         dump_site_1.add_dumper(Dumper(name=f"北露天矿-卸载点1-点位{i}", dumper_cycle_time=1))
-        dump_site_1.show_dumpers()
         dump_site_2.add_dumper(Dumper(name=f"北露天矿-卸载点2-点位{i}", dumper_cycle_time=1))
-        dump_site_2.show_dumpers()
         dump_site_3.add_dumper(Dumper(name=f"北露天矿-卸载点3-点位{i}", dumper_cycle_time=1))
-        dump_site_3.show_dumpers()
+    dump_site_1.show_dumpers()
+    dump_site_2.show_dumpers()
+    dump_site_3.show_dumpers()
+
     # init road
     from sisymines.src.data.example_data import road_3_3,charging_to_load_3_3
     road = Road(road_matrix=road_3_3, charging_to_load_road_matrix=charging_to_load_3_3)
@@ -75,7 +77,7 @@ def main():
     mine.add_road(road)
 
     # test
-    mine.start(total_time=60*1)
+    mine.start(total_time=60*8)
 
 if __name__ == "__main__":
     main()
